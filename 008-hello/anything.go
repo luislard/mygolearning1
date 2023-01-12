0fabb1f9f5b91ce03fd274d71dbee0d78d2e6e4f
Luis Rosales <eng.luisrosales@gmail.com>
2023-01-12 21:36:00 +0100

    I am doing great!

---
+++ 008-hello/anything.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	foo()
+	fmt.Println("Something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+func bar() {
+	fmt.Println("and we exited!")
+}
